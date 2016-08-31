655dd83e968bfb0e2d885e40d9e25379c7ebc5c2
Martin Dreier <martin.dreier@sap.com>
2016-08-31 16:41:11 +0200

    Fix copyright statements

--- src/main/java/de/martindreier/gameoflife/game/io/GridLoader.java
+++ src/main/java/de/martindreier/gameoflife/game/io/GridLoader.java
@@ -1,7 +1,15 @@
-/**
- * Copyright &copy; 2016, SAP SE.
- * All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (C) 2016 Martin Dreier <martin@martindreier.de>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package de.martindreier.gameoflife.game.io;
 
 import java.util.Optional;
@@ -39,7 +47,7 @@ public interface GridLoader {
     /**
      * Return the game rule specified by this loader. The return value may be empty (but not <code>null</code>) if the
      * loader does not specify a game rule.
-     * 
+     *
      * @return The game rule specified by this loader.
      */
     public Optional<GameRule> getGameRule();
